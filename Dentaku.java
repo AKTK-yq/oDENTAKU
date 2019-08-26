041cdf67b373700953cef621d068281a95b8100d
AKTK <yuqikaxe@gmail.com>
2019-08-26 21:49:29 +0900

    最初のコミットですわ。電卓ですわ

---
+++ Dentaku.java
@@ -0,0 +1,207 @@
+import javax.print.attribute.HashAttributeSet;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+import static java.lang.Math.min;
+import static java.lang.String.*;
+import static java.lang.String.valueOf;
+
+public class Dentaku extends JFrame {//クラス
+    //Main以外でも使うからここで宣言
+
+    JPanel contentPane = new JPanel();//パネルの作成
+    JTextField result = new JTextField("0", 36);//計算結果を示すテキスト領域
+
+    //演算子ボタンを押す前にテキスト領域に表示されている数値
+    boolean afterCalc = false;
+    String calcOp = "";
+    double resultValue;
+
+
+    //コンストラクタ
+    public Dentaku() {/*
+        メソッド、関数
+        Mainの構成要素
+        */
+        JPanel panel = new JPanel();//数字用と演算子用のパネル作成
+        JPanel textPanel = new JPanel();//テキスト領域用のパネル作成
+
+        BorderLayout layout1 = new BorderLayout();//新しいボーダレイアウトの構成
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//ｘボタンで終了
+        setTitle("電卓");//タイトル
+        setSize(300, 300);//縦横比
+        setLocationRelativeTo(null);//nullで真ん中表示
+        setContentPane(contentPane);
+        contentPane.setLayout(layout1);
+
+        //contentPaneをフレームのパネルとして設定
+        //テキスト領域を作成
+        textPanel.setLayout(new GridLayout(2, 0));//縦列
+        textPanel.add(result);
+        textPanel.add(new CalcButton("C")).setBackground(new Color(250, 120, 120));
+        contentPane.add(textPanel, BorderLayout.NORTH);//contentPane内の北に配置
+
+        result.setHorizontalAlignment(JTextField.RIGHT);
+
+        //数字ボタンのパネル作成
+        contentPane.add(panel, BorderLayout.CENTER);//contentPane内の真ん中に設置
+        panel.setLayout(new GridLayout(4, 4));//4行4列の分割
+
+        //ボタンを左上から上から順に設置
+        //数字ボタン
+        panel.add(new NumberButton("7"));
+        panel.add(new NumberButton("8"));
+        panel.add(new NumberButton("9"));
+        panel.add(new CalcButton("÷"));
+        panel.add(new NumberButton("4"));
+        panel.add(new NumberButton("5"));
+        panel.add(new NumberButton("6"));
+        panel.add(new CalcButton("×"));
+        panel.add(new NumberButton("1"));
+        panel.add(new NumberButton("2"));
+        panel.add(new NumberButton("3"));
+        panel.add(new CalcButton("－"));
+        panel.add(new NumberButton("0"));
+        panel.add(new NumberButton("00"));
+        panel.add(new NumberButton("."));
+        panel.add(new CalcButton("＋"));
+
+        contentPane.add(new CalcButton("＝"), BorderLayout.SOUTH);//下に配置
+        setVisible(true);//表示・非表示
+    }
+
+    //数字ボタンの定義
+    public class NumberButton extends JButton implements ActionListener {//JButtonの継承とアクション設定
+        //implementsはメソッドに追加、定義とほぼ同意、複数のインターフェースを実装できるActionListenerはインターフェース
+
+        NumberButton(String numberKey) {
+            super(numberKey);//親クラスのコンストラクタJButtonを呼び出す
+            addActionListener(this);//数字ボタンにイベント設定
+            //押した数字がActionListenerObject(thisで自分自身)
+        }
+
+        //アクションが起きた場合（今回はボタンが押されたとき）
+        public void actionPerformed(ActionEvent e) {
+
+            if (afterCalc || result.getText().equals("0") || result.getText().equals("00")) {
+                //演算子が押された後、またはテキスト領域が｢0｣、またはテキスト領域が｢00｣の時
+                if (this.getText().equals(".")) {
+                    //押したボタンが｢.｣の時
+                    result.setText("0.");
+                } else {
+                    //｢.｣じゃないとき
+                    result.setText(this.getText());
+                }
+                afterCalc = false;
+            } else {
+                if (result.getText().contains(".") && this.getText().equals(".") || result.getText().length() >= 15) {
+                    //テキスト領域に｢.｣があるとき、それに加えて押したボタンが｢.｣もしくはテキスト領域の文字数が15文字になっていた時
+                } else {
+                    result.setText(result.getText() + this.getText());
+                }
+            }
+        }
+    }
+
+    //演算子ボタンで利用する変数の定義
+//インスタンスが5個あるため、メソッド内にまとめられない
+    public class CalcButton extends JButton implements ActionListener {
+
+        CalcButton(String calcKey) {
+            super(calcKey);
+            this.addActionListener(this);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+
+            //クリアボタンの設定
+            if (this.getText().equals("C")) {
+                //｢C｣を押した時
+                result.setText("0");
+                resultValue = 0;
+                calcOp = ("");
+                afterCalc = false;
+
+                //演算子ボタンが押されたら
+            } else if (this.getText().equals("＋") || this.getText().equals("－")
+                    || this.getText().equals("×") || this.getText().equals("÷")
+                    || this.getText().equals("＝")) {
+
+                //演算子が格納されている？
+                if (calcOp.equals("＋") || calcOp.equals("－") || calcOp.equals("×") ||
+                        calcOp.equals("÷")) {
+                    double value;
+                    double NEGATIVE_MAX = -100000000000000000d;
+                    double POSITIVE_MAX = 10000000000000000d;
+
+                    value = Double.valueOf(result.getText());
+
+                    if (calcOp.equals("÷")) {
+                        try {
+                            resultValue *= 100;
+                            resultValue /= value;
+                            resultValue /= 100;
+                        } catch (ArithmeticException d) {
+                            resultValue = 0;
+                        }
+                    } else if (calcOp.equals("×")) {
+                        resultValue *= 100;
+                        value *= 100;
+                        resultValue *= value;
+                        resultValue /= 10000;
+                    } else if (calcOp.equals("－")) {
+                        resultValue *= 100;
+                        value *= 100;
+                        resultValue -= value;
+                        resultValue /= 100;
+                    } else if (calcOp.equals("＋")) {
+                        resultValue *= 100;
+                        value *= 100;
+                        resultValue += value;
+                        resultValue /= 100;
+                    }
+
+
+                    if (calcOp.equals("÷") && (Double.valueOf(valueOf(value)) == (0))) {
+                        result.setText("0で割ることはできません");
+                        resultValue = 0;
+                    } else if (resultValue >= POSITIVE_MAX || resultValue <= NEGATIVE_MAX) {
+                        result.setText("値が大きすぎます");
+                        resultValue = 0;
+                    } else {
+                        BigDecimal ResultValue = new BigDecimal(resultValue);
+                        String s = BigDecimal.valueOf(Double.parseDouble(String.valueOf(ResultValue))).toPlainString();
+                        String digits = s.substring(0, min(100, s.length()));
+                        while (digits.substring(digits.length() - 1).equals("0")||digits.substring(digits.length() - 1).equals(".")) {
+                            digits = digits.substring(0, digits.length() - 1);
+                        }
+                        result.setText(digits);
+                    }
+                } else {
+                    //格納されていなかったら
+                    try {
+                        resultValue = Double.valueOf(result.getText());
+                    } catch (NumberFormatException d) {
+                        resultValue = 0;
+                    }
+                }
+            }
+
+            calcOp = this.getText();
+            afterCalc = true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Dentaku();//Javaアプリケーションでの起動
+        //public どのクラスからでもアクセスできる
+        //static クラスに属するメソッドの修飾子　インスタンス化（＝new）せずに使える
+        //void 戻り値なし
+        //main 最初に呼び出されるメソッド、アプリケーション開始メソッド
+    }
+}
---
+++ jikken.java
@@ -0,0 +1,231 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Math.min;
+import static java.lang.String.valueOf;
+
+public class jikken extends JFrame {//クラス
+    //Main以外でも使うからここで宣言
+    JPanel contentPane = new JPanel();//パネルの作成
+    JTextField result = new JTextField("0", 38);//計算結果を示すテキスト領域
+
+    //演算子ボタンを押す前にテキスト領域に表示されている数値
+    boolean afterCalc = false;
+    String calcOp = "";
+    BigDecimal resultValue = BigDecimal.valueOf(0);
+
+    //コンストラクタ
+    public jikken() {/*
+        メソッド、関数
+        Mainの構成要素
+        */
+        JPanel panel = new JPanel();//数字用と演算子用のパネル作成
+        JPanel textPanel = new JPanel();//テキスト領域用のパネル作成
+
+        BorderLayout layout1 = new BorderLayout();//新しいボーダレイアウトの構成
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//ｘボタンで終了
+        setTitle("電卓");//タイトル
+        setSize(300, 300);//縦横比
+        setLocationRelativeTo(null);//nullで真ん中表示
+        setContentPane(contentPane);
+        contentPane.setLayout(layout1);
+        //contentPaneをフレームのパネルとして設定
+        //テキスト領域を作成
+        textPanel.setLayout(new GridLayout(2, 0));//縦列
+        textPanel.add(result);
+        textPanel.add(new CalcButton("C")).setBackground(new Color(250, 119, 118));
+        contentPane.add(textPanel, BorderLayout.NORTH);//contentPane内の北に配置
+        result.setHorizontalAlignment(JTextField.RIGHT);
+        //数字ボタンのパネル作成
+        contentPane.add(panel, BorderLayout.CENTER);//contentPane内の真ん中に設置
+        panel.setLayout(new GridLayout(4, 4));//4行4列の分割
+        //ボタンを左上から上から順に設置
+        //数字ボタン
+        panel.add(new NumberButton("7"));
+        panel.add(new NumberButton("8"));
+        panel.add(new NumberButton("9"));
+        panel.add(new CalcButton("÷"));
+        panel.add(new NumberButton("4"));
+        panel.add(new NumberButton("5"));
+        panel.add(new NumberButton("6"));
+        panel.add(new CalcButton("×"));
+        panel.add(new NumberButton("1"));
+        panel.add(new NumberButton("2"));
+        panel.add(new NumberButton("3"));
+        panel.add(new CalcButton("－"));
+        panel.add(new NumberButton("0"));
+        panel.add(new NumberButton("00"));
+        panel.add(new NumberButton("."));
+        panel.add(new CalcButton("＋"));
+
+        contentPane.add(new CalcButton("＝"), BorderLayout.SOUTH);//下に配置
+        setVisible(true);//表示・非表示
+    }
+
+    //数字ボタンの定義
+    public class NumberButton extends JButton implements ActionListener {//JButtonの継承とアクション設定
+        //implementsはメソッドに追加、定義とほぼ同意、複数のインターフェースを実装できるActionListenerはインターフェース
+
+        NumberButton(String numberKey) {
+            super(numberKey);//親クラスのコンストラクタJButtonを呼び出す
+            addActionListener(this);//数字ボタンにイベント設定
+            //押した数字がActionListenerObject(thisで自分自身)
+        }
+
+        //アクションが起きた場合（今回はボタンが押されたとき）
+        public void actionPerformed(ActionEvent e) {
+
+            if (afterCalc || result.getText().equals("0") || result.getText().equals("00")) {//0と00の時
+                if (this.getText().equals(".")) {//小数点ボタン押したら
+                    result.setText("0.");
+                } else {
+                    result.setText(this.getText());
+                }
+                afterCalc = false;
+            } else {
+
+                int dem = 0;
+                if (result.getText().contains("0.")) {//0.を入力したら
+                    dem += 2;
+                } else if (result.getText().contains(".")) {//.を入力したら
+                    dem += 1;
+                }
+                //小数点は1つまで、文字数は16文字まで(最初の0.や.は数に含まない)
+                if (result.getText().contains(".") && this.getText().equals(".") || result.getText().length() >= 16 + dem) {
+                } else {
+                    result.setText(result.getText() + this.getText());//連結1と0押して10
+                }
+            }
+        }
+    }
+
+    //演算子ボタンで利用する変数の定義
+    //インスタンスが5個あるため、メソッド内にまとめられない
+    public class CalcButton extends JButton implements ActionListener {
+
+        CalcButton(String calcKey) {
+            super(calcKey);
+            this.addActionListener(this);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+
+            //クリアボタンの設定
+            if (this.getText().equals("C")) {
+                //初期化
+                result.setText("0");
+                resultValue = BigDecimal.valueOf(0);
+                calcOp = ("");
+                afterCalc = false;
+
+                //演算子ボタンが押されたら
+            } else if (this.getText().equals("＋") || this.getText().equals("－")
+                    || this.getText().equals("×") || this.getText().equals("÷")
+                    || this.getText().equals("＝")) {
+
+                //演算子が格納されている？
+                if (calcOp.equals("＋") || calcOp.equals("－") || calcOp.equals("×") ||
+                        calcOp.equals("÷")) {
+                    BigDecimal value;
+                    BigDecimal NEGATIVE_plMAX = new BigDecimal("-1E9999");//上限下限
+                    BigDecimal POSITIVE_plMAX = new BigDecimal("1E9999");
+                    BigDecimal NEGATIVE_miMAX = new BigDecimal("-1E-9999");//小数点の上限下限
+                    BigDecimal POSITIVE_miMAX = new BigDecimal("1E-9999");
+
+                    try {
+                        //演算子入力後のテキスト領域の文字を代入
+                        value = new BigDecimal(result.getText());
+                    } catch (NumberFormatException d) {
+                        value = BigDecimal.valueOf(0);
+                    }
+
+                    if (calcOp.equals("÷")) {//格納された演算子が÷だったら
+                        try {
+                            resultValue = resultValue.divide
+                                    (value, 100, BigDecimal.ROUND_DOWN);//割り算を行い、100桁目を切り捨て
+                        } catch (ArithmeticException d) {
+                            resultValue = BigDecimal.valueOf(0);
+                        }
+                    } else if (calcOp.equals("×")) {
+                        resultValue = resultValue.multiply(value);
+                    } else if (calcOp.equals("－")) {
+                        resultValue = resultValue.subtract(value);
+                    } else if (calcOp.equals("＋")) {
+                        resultValue = resultValue.add(value);
+                    }
+
+                    if (calcOp.equals("÷") && (parseDouble(valueOf(value)) == 0)) {
+                        //押された演算子が÷で演算子入力後に代入された値が0だったら
+                        result.setText("0で割ることはできません");
+                        resultValue = BigDecimal.valueOf(0);
+
+                    } else if (resultValue.compareTo(POSITIVE_plMAX) >= 0
+                            || resultValue.compareTo(NEGATIVE_plMAX) <= 0) {//上限値、下限値を超えたら
+                        result.setText("値が大きすぎます");
+                        resultValue = BigDecimal.valueOf(0);
+                    } else if ((resultValue.compareTo(BigDecimal.ZERO) > 0
+                            && resultValue.compareTo(POSITIVE_miMAX) <= 0)
+                            || (resultValue.compareTo(BigDecimal.ZERO) < 0
+                            && resultValue.compareTo(NEGATIVE_miMAX) >= 0)) {//小数点以下の値が多すぎたら
+                        result.setText("値が小さすぎます");
+                    } else {
+                        BigDecimal resultValue2;
+                        //表示用の変数に代入し、16桁-正数で最終桁を四捨五入
+                        resultValue2 = resultValue.setScale
+                                (16 - (resultValue.precision() - resultValue.scale()),
+                                        BigDecimal.ROUND_HALF_EVEN);
+                        //文字列の一番右の値が0だったら一番右の0を取る
+                        if (resultValue2.scale() > 0) {
+                            String resultStr = resultValue2.toPlainString();
+                            String digits = resultStr.substring(0, min(100000, resultStr.length()));
+                            while (digits.substring(digits.length() - 1).equals("0")) {
+                                digits = digits.substring(0, digits.length() - 1);
+                                resultValue2 = new BigDecimal(digits);
+                            }
+                        }
+                        //resultStr2に全てを数字表記にして代入
+                        String resultStr2 = resultValue2.toPlainString();
+                        String digit2 = resultStr2.substring(0, min(100, resultStr2.length()));
+                        //正数で0.0001超えor負数で-0.001未満
+                        if ((resultValue2.compareTo(BigDecimal.valueOf(0.001)) > 0
+                                && resultValue2.compareTo(BigDecimal.valueOf(0)) > 0)
+                                || (resultValue2.compareTo(BigDecimal.valueOf(-0.001)) < 0
+                                && resultValue2.compareTo(BigDecimal.valueOf(0)) < 0)) {
+                            DecimalFormat format1 = new DecimalFormat
+                                    ("0.################");
+                            result.setText(format1.format(resultValue2));
+                        } else if (resultValue2.toPlainString().contains(".") && digit2.length() < 19) {
+                            //数字表記にして表示
+                            result.setText(resultValue2.toPlainString());
+                        } else {
+                            //指数表記にして表示
+                            DecimalFormat format1 = new DecimalFormat
+                                    ("#.#######################################################################################E0");
+                            result.setText(format1.format(resultValue2));
+                        }
+                    }
+                } else {//格納されていなかったら
+                    if (resultValue.compareTo(BigDecimal.valueOf(0)) == 0) {
+                        try {
+                            resultValue = new BigDecimal(result.getText());
+                            //演算子ボタン入力前の値にテキスト領域にある文字列を代入
+                        } catch (NumberFormatException d) {
+                            resultValue = BigDecimal.valueOf(0);
+                        }
+                    }
+                }
+            }
+            calcOp = this.getText();//押された演算子を代入
+            afterCalc = true;//演算子ボタンを押したらtrue
+        }
+    }
+
+    public static void main(String[] args) {
+        new jikken();//Javaアプリケーションでの起動
+    }
+}
