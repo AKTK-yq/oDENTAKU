c072e23867dcc0573c5e4f0d1fbe2a314c4a3ca8
ruzeru <53574422+AKTK-yq@users.noreply.github.com>
2019-08-30 00:13:39 +0900

    コケ・コケコ

--- Dentaku.java
+++ Dentaku.java
@@ -1,4 +1,3 @@
-import javax.print.attribute.HashAttributeSet;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -6,51 +5,92 @@ import java.awt.event.ActionListener;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
+import static java.lang.Double.parseDouble;
 import static java.lang.Math.min;
-import static java.lang.String.*;
 import static java.lang.String.valueOf;
 
 public class Dentaku extends JFrame {//クラス
     //Main以外でも使うからここで宣言
-
-    JPanel contentPane = new JPanel();//パネルの作成
-    JTextField result = new JTextField("0", 36);//計算結果を示すテキスト領域
+    JPanel contentPane = new JPanel(new GridBagLayout());//パネルの作成
+    JPanel textPanel = new JPanel();
+    JTextField result = new JTextField("0", 38);//計算結果を示すテキスト領域
+    JMenuBar menubar = new JMenuBar();
 
     //演算子ボタンを押す前にテキスト領域に表示されている数値
     boolean afterCalc = false;
     String calcOp = "";
-    double resultValue;
+    String calcOp2 = "";
+    BigDecimal resultValue = BigDecimal.ZERO;
+    BigDecimal value = BigDecimal.ZERO;
+    BigDecimal NEGATIVE_plMAX = new BigDecimal("-1E9999");//上限下限
+    BigDecimal POSITIVE_plMAX = new BigDecimal("1E9999");
+    BigDecimal NEGATIVE_miMAX = new BigDecimal("-1E-9999");//小数点の上限下限
+    BigDecimal POSITIVE_miMAX = new BigDecimal("1E-9999");
 
-//こけこけこ
     //コンストラクタ
     public Dentaku() {/*
         メソッド、関数
         Mainの構成要素
         */
         JPanel panel = new JPanel();//数字用と演算子用のパネル作成
-        JPanel textPanel = new JPanel();//テキスト領域用のパネル作成
-
+        // JPanel textPanel = new JPanel();//テキスト領域用のパネル作成
+        JPanel equalPanel = new JPanel();
+        CheckboxGroup cbox = new CheckboxGroup();
         BorderLayout layout1 = new BorderLayout();//新しいボーダレイアウトの構成
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//ｘボタンで終了
         setTitle("電卓");//タイトル
-        setSize(300, 300);//縦横比
+        setSize(400, 400);//縦横比
+        setResizable(false);
         setLocationRelativeTo(null);//nullで真ん中表示
         setContentPane(contentPane);
         contentPane.setLayout(layout1);
-
         //contentPaneをフレームのパネルとして設定
+        Checkbox radio16 = new Checkbox("16進数", false, cbox);//16進数ボタン
+        Checkbox radio10 = new Checkbox("10進数", true, cbox);//10進数ボタン
+        menubar.setLayout(new FlowLayout(FlowLayout.LEFT));//左寄せ
+        menubar.add(radio10);//メニューバーに追加
+        menubar.add(radio16);//同上
+        setJMenuBar(menubar);//画面にセット
         //テキスト領域を作成
-        textPanel.setLayout(new GridLayout(2, 0));//縦列
-        textPanel.add(result);
-        textPanel.add(new CalcButton("C")).setBackground(new Color(250, 120, 120));
+        textPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.weightx = 0.5;
+        gbc.weighty = 1;   // 高さ
+        gbc.fill = GridBagConstraints.BOTH;   // ギャップ(幅)を詰める
+
+        gbc.gridx = 0;//テキストパネル
+        gbc.gridy = 0;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;//最後
+        gbc.fill = GridBagConstraints.HORIZONTAL;//なぜか幅がほとんど伸びない
+        result.setFont(new Font("", Font.PLAIN, 20));
+        textPanel.add(result, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;//なぜか幅がほとんど伸びない
+        CalcButton i = new CalcButton("C");
+        textPanel.add(i, gbc);
+        i.setFont(new Font("", Font.PLAIN, 15));
+        i.setBackground(new Color(250, 120, 120));
+
+
+        int grid_x[] = {0,1, 2, 3, 4, 5};
+        int grid_y[] = {2,2, 2, 2, 2, 2};
+        String buttons[] = {"A", "B", "C", "D", "E","F"};
+
+        for (int f=0; f <6; f++) {
+            JButton button = new NumberButton(buttons[f]);
+            button.setFont(new Font("", Font.PLAIN, 30));
+            gbc.gridwidth = 1;
+            gbc.gridx = grid_x[f];
+            gbc.gridy = grid_y[f];
+            textPanel.add(button,gbc);
+        }
+
         contentPane.add(textPanel, BorderLayout.NORTH);//contentPane内の北に配置
-
         result.setHorizontalAlignment(JTextField.RIGHT);
-
-        //数字ボタンのパネル作成
         contentPane.add(panel, BorderLayout.CENTER);//contentPane内の真ん中に設置
         panel.setLayout(new GridLayout(4, 4));//4行4列の分割
-
         //ボタンを左上から上から順に設置
         //数字ボタン
         panel.add(new NumberButton("7"));
@@ -70,7 +110,11 @@ public class Dentaku extends JFrame {//クラス
         panel.add(new NumberButton("."));
         panel.add(new CalcButton("＋"));
 
-        contentPane.add(new CalcButton("＝"), BorderLayout.SOUTH);//下に配置
+        equalPanel.setLayout(new GridLayout(1, 1));
+        contentPane.add(equalPanel, BorderLayout.SOUTH);
+        equalPanel.add(new CalcButton("＝")).setFont(new Font("", Font.PLAIN, 15));
+
+        ;//下に配置
         setVisible(true);//表示・非表示
     }
 
@@ -87,28 +131,33 @@ public class Dentaku extends JFrame {//クラス
         //アクションが起きた場合（今回はボタンが押されたとき）
         public void actionPerformed(ActionEvent e) {
 
-            if (afterCalc || result.getText().equals("0") || result.getText().equals("00")) {
-                //演算子が押された後、またはテキスト領域が｢0｣、またはテキスト領域が｢00｣の時
-                if (this.getText().equals(".")) {
-                    //押したボタンが｢.｣の時
+            if (afterCalc || result.getText().equals("0") || result.getText().equals("00")) {//0と00の時
+                if (this.getText().equals(".")) {//小数点ボタン押したら
                     result.setText("0.");
                 } else {
-                    //｢.｣じゃないとき
                     result.setText(this.getText());
                 }
                 afterCalc = false;
             } else {
-                if (result.getText().contains(".") && this.getText().equals(".") || result.getText().length() >= 15) {
-                    //テキスト領域に｢.｣があるとき、それに加えて押したボタンが｢.｣もしくはテキスト領域の文字数が15文字になっていた時
+
+                int dem = 0;
+                if (result.getText().contains("0.")) {//0.を入力したら
+                    dem += 2;
+                } else if (result.getText().contains(".")) {//.を入力したら
+                    dem += 1;
+                }
+                //小数点は1つまで、文字数は16文字まで(最初の0.や.は数に含まない)
+                if (result.getText().contains(".") && this.getText().equals(".")
+                        || result.getText().length() >= 16 + dem) {
                 } else {
-                    result.setText(result.getText() + this.getText());
+                    result.setText(result.getText() + this.getText());//連結1と0押して10
                 }
             }
         }
     }
 
     //演算子ボタンで利用する変数の定義
-//インスタンスが5個あるため、メソッド内にまとめられない
+    //インスタンスが5個あるため、メソッド内にまとめられない
     public class CalcButton extends JButton implements ActionListener {
 
         CalcButton(String calcKey) {
@@ -120,88 +169,128 @@ public class Dentaku extends JFrame {//クラス
 
             //クリアボタンの設定
             if (this.getText().equals("C")) {
-                //｢C｣を押した時
+                //初期化
                 result.setText("0");
-                resultValue = 0;
+                resultValue = BigDecimal.valueOf(0);
+                value = BigDecimal.valueOf(0);
                 calcOp = ("");
                 afterCalc = false;
-
                 //演算子ボタンが押されたら
+
             } else if (this.getText().equals("＋") || this.getText().equals("－")
                     || this.getText().equals("×") || this.getText().equals("÷")
                     || this.getText().equals("＝")) {
-
+                //＝が入っていて+-x÷押したら格納していた演算子をいったん削除
+                if (calcOp2.equals("＝")) {
+                    if (!this.getText().equals("＝")) {
+                        calcOp = "";
+                    }
+                } else {
+                    try {
+                        //演算子入力後のテキスト領域の文字を代入
+                        value = new BigDecimal(result.getText());
+                    } catch (NumberFormatException d) {
+                        value = BigDecimal.valueOf(0);
+                    }
+                }
                 //演算子が格納されている？
                 if (calcOp.equals("＋") || calcOp.equals("－") || calcOp.equals("×") ||
                         calcOp.equals("÷")) {
-                    double value;
-                    double NEGATIVE_MAX = -100000000000000000d;
-                    double POSITIVE_MAX = 10000000000000000d;
 
-                    value = Double.valueOf(result.getText());
-
-                    if (calcOp.equals("÷")) {
+                    if (!calcOp2.equals("＝")) {
                         try {
-                            resultValue *= 100;
-                            resultValue /= value;
-                            resultValue /= 100;
-                        } catch (ArithmeticException d) {
-                            resultValue = 0;
+                            //演算子入力後のテキスト領域の文字を代入
+                            value = new BigDecimal(result.getText());
+                        } catch (NumberFormatException d) {
+                            value = BigDecimal.valueOf(0);
                         }
-                    } else if (calcOp.equals("×")) {
-                        resultValue *= 100;
-                        value *= 100;
-                        resultValue *= value;
-                        resultValue /= 10000;
-                    } else if (calcOp.equals("－")) {
-                        resultValue *= 100;
-                        value *= 100;
-                        resultValue -= value;
-                        resultValue /= 100;
-                    } else if (calcOp.equals("＋")) {
-                        resultValue *= 100;
-                        value *= 100;
-                        resultValue += value;
-                        resultValue /= 100;
                     }
-
-
-                    if (calcOp.equals("÷") && (Double.valueOf(valueOf(value)) == (0))) {
+                    switch (calcOp) {
+                        case "÷": //格納された演算子が÷だったら
+                            try {
+                                resultValue = resultValue.divide
+                                        (value, 100, BigDecimal.ROUND_DOWN);//割り算を行い、100桁目を切り捨て
+                            } catch (ArithmeticException d) {
+                                resultValue = BigDecimal.valueOf(0);
+                            }
+                            break;
+                        case "×":
+                            resultValue = resultValue.multiply(value);
+                            break;
+                        case "－":
+                            resultValue = resultValue.subtract(value);
+                            break;
+                        case "＋":
+                            resultValue =
+                                    resultValue.add(value);
+                            break;
+                    }
+                    if (calcOp.equals("÷") && (parseDouble(valueOf(value)) == 0)) {
+                        //押された演算子が÷で演算子入力後に代入された値が0だったら
                         result.setText("0で割ることはできません");
-                        resultValue = 0;
-                    } else if (resultValue >= POSITIVE_MAX || resultValue <= NEGATIVE_MAX) {
+                        resultValue = BigDecimal.valueOf(0);
+                    } else if (resultValue.compareTo(POSITIVE_plMAX) >= 0
+                            || resultValue.compareTo(NEGATIVE_plMAX) <= 0) {//上限値、下限値を超えたら
                         result.setText("値が大きすぎます");
-                        resultValue = 0;
+                        resultValue = BigDecimal.valueOf(0);
+                    } else if ((resultValue.compareTo(BigDecimal.ZERO) > 0
+                            && resultValue.compareTo(POSITIVE_miMAX) <= 0)//0超えで上限桁数以下
+                            || (resultValue.compareTo(BigDecimal.ZERO) < 0
+                            && resultValue.compareTo(NEGATIVE_miMAX) >= 0)) {//0未満で上限桁数以下
+                        result.setText("値が小さすぎます");
+                        resultValue = BigDecimal.valueOf(0);
                     } else {
-                        BigDecimal ResultValue = new BigDecimal(resultValue);
-                        String s = BigDecimal.valueOf(Double.parseDouble(String.valueOf(ResultValue))).toPlainString();
-                        String digits = s.substring(0, min(100, s.length()));
-                        while (digits.substring(digits.length() - 1).equals("0")||digits.substring(digits.length() - 1).equals(".")) {
-                            digits = digits.substring(0, digits.length() - 1);
+                        BigDecimal resultValue2;
+                        //表示用の変数に代入し、16桁-正数で最終桁を四捨五入
+                        resultValue2 = resultValue.setScale
+                                (16 - (resultValue.precision() - resultValue.scale()),
+                                        BigDecimal.ROUND_HALF_EVEN);
+                        //文字列の一番右の値が0もしくは結果が0だったら一番右の0を取る
+                        if (resultValue2.scale() > 0 || resultValue2.compareTo(BigDecimal.ZERO) == 0) {
+                            String resultStr = resultValue2.toPlainString();
+                            resultStr = resultStr.substring(0, min(10000, resultStr.length()));
+                            try {
+                                while (resultStr.substring(resultStr.length() - 1).equals("0")) {
+                                    resultStr = resultStr.substring(0, resultStr.length() - 1);
+                                }
+                            } catch (StringIndexOutOfBoundsException f) {
+                                resultValue = BigDecimal.ZERO;
+                                resultStr = "0";
+                            }
+                            resultValue2 = new BigDecimal(resultStr);
                         }
-                        result.setText(digits);
+                        //resultStr2に全てを数字表記にして代入
+                        //正数で0.0001超えand99~9未満or負数で-0.001未満and-99~9超え
+                        if ((resultValue2.compareTo(BigDecimal.valueOf(0.001)) > 0
+                                && resultValue2.compareTo(BigDecimal.valueOf(9999999999999999d)) < 0)
+                                || (resultValue2.compareTo(BigDecimal.valueOf(-0.001)) < 0
+                                && resultValue2.compareTo(BigDecimal.valueOf(-9999999999999999d)) > 0)
+                                || resultValue2.equals(BigDecimal.ZERO)) {
+                            result.setText(resultValue2.toPlainString());
+                        } else {
+                            //指数表記にして表示
+                            DecimalFormat format1 = new DecimalFormat("#.###############E0");
+                            result.setText(format1.format(resultValue2));
+                        }
                     }
-                } else {
-                    //格納されていなかったら
+                } else {//格納されていなかったら
                     try {
-                        resultValue = Double.valueOf(result.getText());
+                        resultValue = new BigDecimal(result.getText());
+                        //演算子ボタン入力前の値にテキスト領域にある文字列を代入
                     } catch (NumberFormatException d) {
-                        resultValue = 0;
+                        resultValue = BigDecimal.valueOf(0);
                     }
                 }
+                if (!this.getText().equals("＝")) {
+                    calcOp = this.getText();//押された演算子を代入
+                }
             }
-
-            calcOp = this.getText();
-            afterCalc = true;
+            calcOp2 = this.getText();
+            afterCalc = true;//演算子ボタンを押したらtrue
         }
-
     }
 
     public static void main(String[] args) {
         new Dentaku();//Javaアプリケーションでの起動
-        //public どのクラスからでもアクセスできる
-        //static クラスに属するメソッドの修飾子　インスタンス化（＝new）せずに使える
-        //void 戻り値なし
-        //main 最初に呼び出されるメソッド、アプリケーション開始メソッド
     }
-}//kokekokeko
+}
